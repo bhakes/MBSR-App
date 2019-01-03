69d8edec95554eec54a637dbcdfe8c12fe66b790
Ben Hakes <bhhakes@gmail.com>
2019-01-03 17:47:29 -0600

    add PracticesCVCell

---
+++ MBSR-App/View Controllers/PracticesCollectionViewController/PracticesCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PracticesCollectionViewCell.swift
+//  MBSR-App
+//
+//  Created by Benjamin Hakes on 1/3/19.
+//  Copyright © 2019 Benjamin Hakes. All rights reserved.
+//
+
+import UIKit
+
+class PracticesCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
