7d7133e99919f240bd78aed9d26da4e6106e67f9
Ben Hakes <bhhakes@gmail.com>
2019-01-03 12:02:47 -0600

    update journal view controller with journal names

--- MBSR-App/View Controllers/JournalViewController/JournalTableViewController.swift
+++ MBSR-App/View Controllers/JournalViewController/JournalTableViewController.swift
@@ -45,9 +45,9 @@ class JournalTableViewController: UITableViewController {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath) as? JournalTableViewCell else {fatalError("could not dequeue Journal cell")}
         
         if indexPath.section == 0 {
-            cell.textLabel?.text = "1"
+            cell.textLabel?.text = "Weeks 1-7 - Formal Practice Log"
         } else {
-            cell.textLabel?.text = "2"
+            cell.textLabel?.text = " Week \(indexPath.row + 1) - \(MSBRContent.InformalPractices.allCases[indexPath.row].rawValue)"
         }
         // Configure the cell...
 
@@ -62,40 +62,6 @@ class JournalTableViewController: UITableViewController {
             return "Informal"
         }
     }
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
