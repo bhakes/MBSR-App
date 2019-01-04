34052ad05e8ede2f64b1f22d3e5e472e889a19fc
Jason Modisett <jason@jasons-macbook-pro.local>
2019-01-04 03:25:47 -0600

    Added Instantiate third party library

--- MBSR-App/View Controllers/PracticesCollectionViewController/PracticesCollectionViewController.swift
+++ MBSR-App/View Controllers/PracticesCollectionViewController/PracticesCollectionViewController.swift
@@ -10,84 +10,37 @@ import UIKit
 
 class PracticesCollectionViewController: UICollectionViewController {
     
+    // MARK:- View lifecycle methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Practices"
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
         
-        // Register cell classes
-        self.collectionView.register(UINib(nibName: "PracticesCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "practicesCell")
-        // Do any additional setup after loading the view.
+        collectionView.register(UINib(nibName: "PracticesCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "practicesCell")
     }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using [segue destinationViewController].
-     // Pass the selected object to the new view controller.
-     }
-     */
     
-    // MARK: UICollectionViewDataSource
-    
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-    
+    // MARK:- UICollectionViewDataSource
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
         return MSBRContent.shared.practices.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PracticesCollectionViewCell.reuseIdentifier, for: indexPath) as! PracticesCollectionViewCell
         
         if let textLabelString = MSBRContent.shared.practices[indexPath.row]["title"] as? String {
             cell.titleLabel.text = textLabelString
         }
+        
         if let image = MSBRContent.shared.practices[indexPath.row]["image"] as? String {
             cell.imageView.image = UIImage(named: image)
         }
         
-        // Configure the cell
-        
         return cell
     }
     
-    // MARK: UICollectionViewDelegate
     
-    /*
-     // Uncomment this method to specify if the specified item should be highlighted during tracking
-     override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-     return true
-     }
-     */
-    
-    /*
-     // Uncomment this method to specify if the specified item should be selected
-     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-     return true
-     }
-     */
-    
-    /*
-     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-     override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-     return false
-     }
-     
-     override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-     return false
-     }
-     
-     override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-     
-     }
-     */
+    // MARK:- UICollectionViewDelegate
     
 }
